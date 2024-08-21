29fff93a7677510d779f968ae5882ef7a7b3f236
Dario Zubillaga <dario@dazu.ma>
2024-08-21 16:33:07 -0600

    test: envs

--- src/components/AdminLayout/AdminLayout.tsx
+++ src/components/AdminLayout/AdminLayout.tsx
@@ -14,7 +14,7 @@ async function AdminLayout({ children }: PropsWithChildren) {
         <div className="mx-auto flex h-full max-w-screen-lg flex-col gap-4 p-4">
           <div className="flex w-full items-center justify-between border-b py-2">
             <div className="flex items-center">
-              <p className="font-extrabold">{`${session?.user?.name} (${process.env.NETLIFY}) (${process.env.CONTEXT})`}</p>
+              <p className="font-extrabold">{`${session?.user?.name} (${process.env.NODE_ENV})`}</p>
 
               {isAdmin && (
                 <div className="items ml-4 flex h-full items-center gap-4 border-l py-2 pl-4">
